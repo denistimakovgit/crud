6d754e5477734071495d83f3b775f21d90ddb75d
Denis test <denistimakow@gmail.com>
2023-09-04 01:36:56 +0300

    initial commit

---
+++ stocks_products/logistic/views.py
@@ -0,0 +1,21 @@
+from rest_framework.pagination import LimitOffsetPagination
+from rest_framework.viewsets import ModelViewSet
+
+from logistic.models import Product, Stock
+from logistic.serializers import ProductSerializer, StockSerializer
+from django_filters.rest_framework import DjangoFilterBackend
+
+
+class ProductViewSet(ModelViewSet):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+    # при необходимости добавьте параметры фильтрации
+
+
+class StockViewSet(ModelViewSet):
+    queryset = Stock.objects.all()
+    serializer_class = StockSerializer
+
+    filter_backends = [DjangoFilterBackend]
+    filterset_fields = ['products']
+    pagination_class = LimitOffsetPagination
