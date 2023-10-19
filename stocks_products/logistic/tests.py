1bef06b29e0578df4a46fac247a28291ba557222
denistimakovgit <106560716+denistimakovgit@users.noreply.github.com>
2023-10-20 01:46:44 +0300

    Update tests.py

--- stocks_products/logistic/tests.py
+++ stocks_products/logistic/tests.py
@@ -1,10 +1,8 @@
 from django.test import TestCase
 from rest_framework.test import APIClient
 
-# Create your tests here.
-class TestSmth(TestCase):
 
-    
+class TestSmth(TestCase):
     def test_sample_view_ok(self):
         client = APIClient()
         url = 'api/v1/products/'
