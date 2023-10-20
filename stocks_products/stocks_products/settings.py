9f059893afe9eea0389ecd94a7b8e44666ec9dae
denistimakovgit <106560716+denistimakovgit@users.noreply.github.com>
2023-10-21 01:10:41 +0300

    Update settings.py

--- stocks_products/stocks_products/settings.py
+++ stocks_products/stocks_products/settings.py
@@ -80,12 +80,12 @@ WSGI_APPLICATION = 'stocks_products.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'netology_stocks_products',
-        'HOST': 'localhost',
-        'PORT': '5432',
-        #'USER': os.getenv('DB_USER'),
-        #'PASSWORD': os.getenv('DB_PASSWORD'),
+        'ENGINE': os.getenv('DB_ENGINE'),
+        'NAME': os.getenv('DB_NAME'),
+        'HOST': os.getenv('DB_HOST'),
+        'PORT': os.getenv('DB_PORT'),
+        'USER': os.getenv('DB_USER'),
+        'PASSWORD': os.getenv('DB_PASSWORD'),       
     }
 }
 
